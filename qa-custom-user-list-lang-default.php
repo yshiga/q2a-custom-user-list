7531ab03c34f891d556094b2b8b500530dd08c10
yaimapp <gmonday730-mbp@yahoo.co.jp>
2017-01-26 11:34:14 +0900

    言語ファイル追加

---
+++ qa-custom-user-list-lang-default.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+    // default japanese
+    'location_title' => '^1の養蜂家',
+    'location_description' => '^1で活動されている、ニホンミツバチのQ&Aを利用されている養蜂家の一覧です。^1で養蜂仲間を増やしてください。',
+);
+
+/*
+    Omit PHP closing tag to help avoid accidental output
+*/

3479fd587edd8548ee7fcb3a6341ae93b07a373e
yaimapp <gmonday730-mbp@yahoo.co.jp>
2017-09-01 10:51:41 +0900

    無限スクロール関連削除

--- qa-custom-user-list-layer.php
+++ qa-custom-user-list-layer.php
@@ -4,19 +4,16 @@ require_once CUL_DIR.'/cul-theme-main.php';
 
 class qa_html_theme_layer extends qa_html_theme_base
 {
-    private $infscr = null;
     
     function qa_html_theme_layer($template, $content, $rooturl, $request)
     {
         qa_html_theme_base::qa_html_theme_base($template, $content, $rooturl, $request);
-        $this->infscr = new qa_infinite_scroll();
     }
     
     function body_footer()
     {
         qa_html_theme_base::body_footer();
         if (qa_opt('site_theme') === CUL_TARGET_THEME_NAME && $this->template === 'users') {
-            $this->output('<SCRIPT TYPE="text/javascript" SRC="'.$this->infscr->pluginjsurl.'jquery-ias.min.js"></SCRIPT>');
             $cul_lang_json = json_encode (array(
               'read_next' => qa_lang_html('cul_lang/read_next'),
               'read_previous' => qa_lang_html('cul_lang/read_previous'),
@@ -27,7 +24,6 @@ class qa_html_theme_layer extends qa_html_theme_base
               "var cul_lang = '".$cul_lang_json."';",
               '</SCRIPT>'
             );
-            $this->output('<SCRIPT TYPE="text/javascript" SRC="'. QA_HTML_THEME_LAYER_URLTOROOT.'js/ias-users.js"></SCRIPT>');
             
             $cond_location = qa_get('location');
             $this->output('<SCRIPT>',' var cond_location = "'.$cond_location.'";','</SCRIPT>');
@@ -35,14 +31,6 @@ class qa_html_theme_layer extends qa_html_theme_base
         }
     }
 
-    function head_css()
-    {
-        qa_html_theme_base::head_css();
-        if (qa_opt('site_theme') === CUL_TARGET_THEME_NAME && $this->template === 'users') {
-            $this->output('<LINK REL="stylesheet" TYPE="text/css" HREF="'.$this->infscr->plugincssurl.'jquery.ias.css"/>');
-        }
-    }
-    
     public function main()
     {
         if (qa_opt('site_theme') === CUL_TARGET_THEME_NAME && $this->template === 'users') {
